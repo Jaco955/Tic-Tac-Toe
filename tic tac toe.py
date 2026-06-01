6737289aaf182deb8c52ff3f760738920d68813e
JJ <jacotareas@gmail.com>
2026-05-31 20:10:04 -0500

    update on reset func

--- tic tac toe.py
+++ tic tac toe.py
@@ -4,20 +4,30 @@ from tkinter import messagebox
 
 
 def ButtonClick(button):
-    global x_o, flag
+    
+    global x_o, flag, active
     button["bg"] = "#2ec4b6"
+    
+
+    print(active)
     if button["text"] == "" and x_o ==True:
         button["text"] = "X"
         x_o = False
-        checkforwin()
+        if(checkforwin()):
+            reset()
         flag = flag+1
     elif button["text"] == "" and x_o ==False:
         button["text"] = "O"
         x_o = True
-        checkforwin()
+        if(checkforwin()):
+            reset()
         flag = flag+1
     else:
         messagebox.showinfo("Tic Tac Toe", "Player inside alr gng")
+            
+
+    
+
 
 
 """
@@ -33,44 +43,70 @@ def ButtonClick(button):
 
 
 def checkforwin():
-    global button1, button2, button3, button4, button5, button6, button7, button8, button9
+    global button1, button2, button3, button4, button5, button6, button7, button8, button9, active
     if button1["text"] == "X" and button2["text"] =="X" and button3["text"] =="X" or button4["text"] =="X" and button5["text"] =="X" and button6["text"] =="X" or button7["text"] =="X" and button8["text"] =="X" and button9["text"] =="X" or button1["text"] =="X" and button5["text"] =="X" and button9["text"] =="X" or button3 ["text"] =="X"  and button5["text"] =="X" and button7["text"] =="X" or button1["text"] =="X" and button4["text"] =="X" and button7["text"] =="X" or button2["text"] =="X" and button5["text"] =="X" and button8["text"] =="X" or button3["text"] =="X" and button6["text"] =="X" and button9["text"] =="X":
-        messagebox.showinfo ("Tic Tac Toe", "Player X wins, Player O learn how to play")
+        active = False
+        return messagebox.askyesno ("Tic Tac Toe", "Player X wins, Player O learn how to play. Play again?")
     elif button1["text"] == "O" and button2["text"] =="O" and button3["text"] =="O" or button4["text"] =="O" and button5["text"] =="O" and button6["text"] =="O" or button7["text"] =="O" and button8["text"] =="O" and button9["text"] =="O" or button1["text"] =="O" and button5["text"] =="O" and button9["text"] =="O" or button3 ["text"] =="O"  and button5["text"] =="O" and button7["text"] =="O" or button1["text"] =="O" and button4["text"] =="O" and button7["text"] =="O" or button2["text"] =="O" and button5["text"] =="O" and button8["text"] =="O" or button3["text"] =="O" and button6["text"] =="O" and button9["text"] =="O":
-        messagebox.showinfo ("Tic Tac Toe", "Player O wins, Womp Womp Player X")
+        
+        active = False
+        return messagebox.askyesno ("Tic Tac Toe", "Player O wins, Womp Womp Player X. Play again?")
     elif flag == 8:
-        messagebox.showinfo ("Game tied", "Try again dudes")   
+        active = False
+        return messagebox.askyesno ("Game tied", "Try again dudes. Play again?")
+        
+
 main = Tk()
 main.title("Tic tac toe")
 
-x_o = True
+def reset():
+    global x_o, flag, active
+    global button1, button2, button3, button4, button5, button6, button7, button8, button9
 
+    x_o = True
+    flag = 0
+    active = True
+
+    botones = [button1, button2, button3,button4, button5, button6,button7, button8, button9]
+
+    for boton in botones:
+        boton["text"] = ""
+        boton["bg"] = "#e0ffa7"
+    
+active = True    
+x_o = True
 flag = 0
+
+
 button1 = Button(main,text="", font=("arial", 60,"bold"), bg="#e0ffa7", fg="white", width=3, command=lambda: ButtonClick(button1))
-button1.grid(row=0, column=0)
+button1.grid(row=1, column=0)
 
 button2 = Button(main,text="", font=("arial", 60,"bold"), bg="#e0ffa7", fg="white", width=3, command=lambda: ButtonClick(button2))
-button2.grid(row=0, column=1)
+button2.grid(row=1, column=1)
 
 button3 = Button(main,text="", font=("arial", 60,"bold"), bg="#e0ffa7", fg="white", width=3, command=lambda: ButtonClick(button3))
-button3.grid(row=0, column=2)
+button3.grid(row=1, column=2)
 
 button4 = Button(main,text="", font=("arial", 60,"bold"), bg="#e0ffa7", fg="white", width=3, command=lambda: ButtonClick(button4))
-button4.grid(row=1, column=0)
+button4.grid(row=2, column=0)
 
 button5 = Button(main,text="", font=("arial", 60,"bold"), bg="#e0ffa7", fg="white", width=3, command=lambda: ButtonClick(button5))
-button5.grid(row=1, column=1)
+button5.grid(row=2, column=1)
 
 button6 = Button(main,text="", font=("arial", 60,"bold"), bg="#e0ffa7", fg="white", width=3, command=lambda: ButtonClick(button6))
-button6.grid(row=1, column=2)
+button6.grid(row=2, column=2)
 
 button7 = Button(main,text="", font=("arial", 60,"bold"), bg="#e0ffa7", fg="white", width=3, command=lambda: ButtonClick(button7))
-button7.grid(row=2, column=0)
+button7.grid(row=3, column=0)
 
 button8 = Button(main,text="", font=("arial", 60,"bold"), bg="#e0ffa7", fg="white", width=3, command=lambda: ButtonClick(button8))
-button8.grid(row=2, column=1)
+button8.grid(row=3, column=1)
 
 button9 = Button(main,text="", font=("arial", 60,"bold"), bg="#e0ffa7", fg="white", width=3, command=lambda: ButtonClick(button9))
-button9.grid(row=2, column=2)
+button9.grid(row=3, column=2)
+
+buttonrestart = Button(main,text="Restart", font=("arial", 24,"bold"), bg="#e0ffa7", fg="black", width=8, command=lambda: reset())
+buttonrestart.grid(row=4, column=1)
+
 
 main.mainloop()
